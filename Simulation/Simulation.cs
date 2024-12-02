d225dfff76a51de34622baefec33ad4c2abe11e9
Christopher Larson <christroy.larson@gmail.com>
2024-12-01 22:05:44 -0700

    Updated the simulation

---
+++ Simulation/Simulation.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Simulation
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D _circleTexture;
+        private Texture2D _planeTexture;
+        private Vector2 _circlePosition;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _circlePosition = new Vector2(GraphicsDevice.Viewport.Width / 2f, GraphicsDevice.Viewport.Height / 2f);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _circleTexture = CreateCircleTexture(50, Color.Red); // Radius = 50
+
+            _planeTexture = new Texture2D(GraphicsDevice, 1, 1);
+            _planeTexture.SetData(new[] { Color.Gray });
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(_circleTexture, _circlePosition - new Vector2(50, 50), Color.White);
+
+            _spriteBatch.Draw(_planeTexture, new Rectangle(0, GraphicsDevice.Viewport.Height - 20, GraphicsDevice.Viewport.Width, 20), Color.Gray);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private Texture2D CreateCircleTexture(int radius, Color color)
+        {
+            int diameter = radius * 2;
+            Texture2D texture = new Texture2D(GraphicsDevice, diameter, diameter);
+
+            Color[] colorData = new Color[diameter * diameter];
+            Vector2 center = new Vector2(radius, radius);
+
+            for (int y = 0; y < diameter; y++)
+            {
+                for (int x = 0; x < diameter; x++)
+                {
+                    Vector2 position = new Vector2(x, y);
+                    if (Vector2.Distance(position, center) <= radius)
+                    {
+                        colorData[y * diameter + x] = color;
+                    }
+                    else
+                    {
+                        colorData[y * diameter + x] = Color.Transparent;
+                    }
+                }
+            }
+
+            texture.SetData(colorData);
+            return texture;
+        }
+    }
+}
